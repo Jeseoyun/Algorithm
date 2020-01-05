50ff166635bf5f7b4fea80088e7e428c2ead59aa
Jeseoyun <56038425+Jeseoyun@users.noreply.github.com>
2020-01-06 02:44:59 +0900

    Update 10989_sort.cpp

--- 10989_sort.cpp
+++ 10989_sort.cpp
@@ -1,6 +1,6 @@
 /*
  * countingSort.cpp
- * 백준 10989번_수 정렬하기3(counting sort)_미완성(time out)
+ * 백준 10989번_수 정렬하기3(counting sort)
  *
  *  Created on: 2020. 1. 5.
  *      Author: 제서윤
@@ -8,25 +8,47 @@
 
 
 #include <iostream>
-#define MAX 10000
 using namespace std;
+
+void countingSort(int a[], int n){
+	int maxi = 0;
+	for(int i=0; i<n; i++)
+		if(a[i]>maxi)
+			maxi = a[i];
+
+	int count[maxi] = {};
+	for(int i=0; i<n; i++)
+		count[a[i]-1]++;
+
+	for(int j=0; j<maxi; j++)
+		count[j] = count[j] + count[j-1];
+
+	int sorted[maxi] = {};
+	for(int i=n-1; i>=0; i--){
+		sorted[count[a[i]-1]-1] = a[i];
+		count[a[i]-1]--;
+	}
+	for(int j=0; j<n; j++)
+		a[j] = sorted[j];
+}
 
 int main(){
 	int n;
 	cin >> n;
 
 	int num[n];
+
 	for(int i=0; i<n; i++)
 		cin >> num[i];
 
-	int count[MAX];
+	int count[n]={};
 	for(int i=0; i<n; i++)
 		count[num[i]-1]++;
 
-	for(int i=0; i<MAX; i++){
+	for(int i=0; i<n; i++){
 		if(count[i] != 0)
 			for(int j=0; j<count[i]; j++)
-				cout << i+1;
+				cout << i+1 << "\n";
 	}
 
 	return 0;
