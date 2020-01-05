fb7d1e7e28e95e66efe5820694b4c68018a227af
Jeseoyun <56038425+Jeseoyun@users.noreply.github.com>
2020-01-06 03:05:32 +0900

    Update 10989_sort.cpp

--- 10989_sort.cpp
+++ 10989_sort.cpp
@@ -1,6 +1,6 @@
 /*
  * countingSort.cpp
- * 백준 10989번_수 정렬하기3(counting sort)_error_메모리초과
+ * 백준 10989번_수 정렬하기3(counting sort)
  *
  *  Created on: 2020. 1. 5.
  *      Author: 제서윤
@@ -8,48 +8,27 @@
 
 
 #include <iostream>
+#define MAX 10001
 using namespace std;
 
-void countingSort(int a[], int n){
-	int maxi = 0;
-	for(int i=0; i<n; i++)
-		if(a[i]>maxi)
-			maxi = a[i];
-
-	int count[maxi] = {};
-	for(int i=0; i<n; i++)
-		count[a[i]-1]++;
-
-	for(int j=0; j<maxi; j++)
-		count[j] = count[j] + count[j-1];
-
-	int sorted[maxi] = {};
-	for(int i=n-1; i>=0; i--){
-		sorted[count[a[i]-1]-1] = a[i];
-		count[a[i]-1]--;
-	}
-	for(int j=0; j<n; j++)
-		a[j] = sorted[j];
-}
-
 int main(){
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
 	int n;
 	cin >> n;
 
-	int num[n];
-
-	for(int i=0; i<n; i++)
-		cin >> num[i];
-
-	int count[n]={};
-	for(int i=0; i<n; i++)
-		count[num[i]-1]++;
-
+	int count[MAX]={};
 	for(int i=0; i<n; i++){
-		if(count[i] != 0)
-			for(int j=0; j<count[i]; j++)
-				cout << i+1 << "\n";
+		int input;
+		cin >> input;
+		count[input]++;
 	}
 
+	for(int i=0; i<MAX; i++){
+		if(count[i])
+			for(int j=0; j<count[i]; j++)
+				cout << i << "\n";
+	}
 	return 0;
 }
