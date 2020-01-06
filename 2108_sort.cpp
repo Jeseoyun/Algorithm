d1148443704481c350295e570da7f10b156d882a
Jeseoyun <56038425+Jeseoyun@users.noreply.github.com>
2020-01-07 01:20:04 +0900

    Update 2108_sort.cpp

--- 2108_sort.cpp
+++ 2108_sort.cpp
@@ -1,6 +1,6 @@
 /*
  * statistics.cpp
- * 백준 2108번_통계학_미완성
+ * 백준 2108번_통계학
  *
  *  Created on: 2020. 1. 6.
  *      Author: 제서윤
@@ -8,43 +8,58 @@
 
 
 #include <iostream>
-#include <sort>
+#include <math.h>
+#include <algorithm>
 using namespace std;
 
 int main(){
 	int n;	//홀수
 	cin >> n;
-	
+
 	int num[n];
 	for(int i=0; i<n; i++)
 		cin >> num[i];
-	
+
 	sort(num, num+n);
-	
-	int mean;	//산술평균
+
+	int mean=0;	//산술평균
 	for(int i=0; i<n; i++)
 		mean += num[i];
-	cout << mean*(10.0)/n/10 << endl;
-	
+	cout << floor(mean*(1.0)/n+0.5) << endl;
+
 	int mid;	//중앙값
-	mid = num[(n-1/2)];
+	mid = num[(n-1)/2];
 	cout << mid << endl;
-	
-	int mode=num[0];	//최빈값
-	int count=1;
-	for(int i=0; i<n-1; i++){
-		if(num[i]==num[i+1])
-			count++;
-		if(count>mode){
-			mode = i;
-			count = 1;
-		}
+
+	int mode;	//최빈값
+	int count[n] = {};
+	for(int i=0; i<n; i++)
+		count[i] = 1;
+	for(int i=1; i<n; i++){
+		if(num[i-1]==num[i])
+			count[i] = count[i-1]+1;
 	}
-	
+	int maxi = 0;
+	for(int i=0; i<n; i++){
+		if(count[i]>=maxi)
+			maxi = count[i];
+	}
+	int loop = 0;
+	for(int i=0; i<n; i++)
+		if(count[i]==maxi){
+			mode = num[i];
+			loop++;
+			if(loop == 2)
+				break;
+		}
+
+	cout << mode << endl;
+
 	int range;	//범위
-	
+	range = num[n-1]-num[0];
+	cout << range;
 
 	return 0;
-	
+
 }
 
