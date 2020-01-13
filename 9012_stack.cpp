d90d97706ede33853163734bf0bcb1d1eb2f9323
Jeseoyun <56038425+Jeseoyun@users.noreply.github.com>
2020-01-13 18:47:54 +0900

    Update 9012_stack.cpp

--- 9012_stack.cpp
+++ 9012_stack.cpp
@@ -15,24 +15,37 @@ int main(){
 	int T;
 	cin >> T;
 
-	stack<char> galho;
 	for(int i=0; i<T; i++){
 		string s;
 		cin >> s;
-		for(int j=0; j<s.length(); j++){
-			if(s[j]=='(')
+		stack<char> galho;
+		for(int j=0; j<(int)s.length(); j++){
+			if(s[j]=='('){
 				galho.push(s[j]);
+				//cout << "push " << endl;
+				//cout << "stack size : " << galho.size() << endl;
+			}
 			else if(s[j]==')'){
 				if(galho.empty()){
-					cout << "NO" << endl;
+					galho.push('x');
+					//여는괄호가 남아있지 않은 상황에서 닫는괄호가 나온 경우
+					//임의로 x라는 문자를 스택에 추가한 후 반복문을 탈출하도록 하여 반복문 밖의 if문에서 스택이 비어있지 않은 경우로 들어가 NO를 출력
 					break;
 				}
 				galho.pop();
+				//cout << "pop" << endl;
+				//cout << "stack size : " << galho.size() << endl;
 			}
 			else
 				continue;
 		}
-	}
+		if(galho.empty())
+			cout << "YES" << endl;
+		else
+			cout << "NO" << endl;
 
+		//cout << "---------------" << endl;
+	}
+	return 0;
 }
 
