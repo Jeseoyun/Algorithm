4ab235b13a6d272a6beeb79e0dc41dc414d75db7
Jeseoyun <56038425+Jeseoyun@users.noreply.github.com>
2020-01-25 21:03:04 +0900

    Update 10250_math.cpp

--- 10250_math.cpp
+++ 10250_math.cpp
@@ -1,18 +1,12 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 int main(){
 	int t;
-	cin >> t;
-
+	scanf("%d", &t);
 	while(t--){
 		int h, w, n;	//전체 h층, 각 층 w개의 방, n번째손님
-		cin >> h >> w >> n;
-		int room;
-		if(n%h == 0)
-			room = h*100 + n/h;
-		else
-			room = (n/h)+1 + (n%h)*100;
-		cout << room << "\n";
+		scanf("%d %d %d", &h, &w, &n);
+		int room = n%h==0? h*100+n/h:(n%h)*100+(n/h)+1;
+		printf("%d \n", room);
 	}
 	return 0;
 }
