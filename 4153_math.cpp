8b1f03c4d191f23e22d162c56e9adf607c779398
Jeseoyun <56038425+Jeseoyun@users.noreply.github.com>
2020-01-28 00:42:48 +0900

    Create 4153_math.cpp

---
+++ 4153_math.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+	while(1){
+		int side[3];
+		cin >> side[0] >> side[1] >> side[2];
+		if(side[0]*side[1]*side[2]==0)
+			break;
+		sort(side, side+3);
+		if(side[0]*side[0]+side[1]*side[1] == side[2]*side[2])
+			cout << "right" << "\n";
+		else
+			cout << "wrong" << "\n";
+	}
+	return 0;
+}
